9a67877797aa4b1de365a53a2ebd2e06ad5daffb
alexander rosen <alexander.rosen@live.se>
2019-09-18 13:04:45 +0200

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './home/carousel/carousel.component';
+import { FeaturesComponent } from './home/features/features.component';
+import { ProductsComponent } from './home/products/products.component';
+import { ReviewsComponent } from './home/reviews/reviews.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ProductpageComponent } from './productpage/productpage.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    FeaturesComponent,
+    ProductsComponent,
+    ReviewsComponent,
+    NavComponent,
+    FooterComponent,
+    AboutComponent,
+    ProductpageComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
